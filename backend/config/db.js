51155c9b32991261577c0473e5bc43590ee94d44
Aashish Gupta <aashishgupta99300@gmail.com>
2025-12-03 13:02:43 +0530

    c

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose")
-let isconnected = false;
+
 
 async function connectDB() {
     try{
         await mongoose.connect(process.env.MONGODB_URI);
-        isconnected = true;
     }catch(err){
         console.log(err);
     }
 }
 
-module.exports =connectDB;
+module.exports =connectDB;  
